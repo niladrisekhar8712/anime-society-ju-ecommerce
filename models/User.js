4168c566a15e61c615a49da6dcab9cd4c102fb20
Niladri Sekhar Mondal <115944239+niladrisekhar8712@users.noreply.github.com>
2025-03-09 17:21:18 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     _id: {type: String, required: true},
     name: {type: String, required: true},
-    email: {type: String, required: true, unique},
+    email: {type: String, required: true, unique: true},
     imageUrl: {type: String, required: true},
     phoneNo: {type: String}, // for the time being etake not required rakhlam
     cartItems: {type: Object, default: {}},
